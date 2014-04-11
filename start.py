b014d9502e26384473c6907a8acba5ab17227568
Mikhail Melnik <by.zumzoom@gmail.com>
2014-04-11 23:07:29 +0400

    Multiply input files support

--- start.py
+++ start.py
@@ -16,6 +16,8 @@ DEFAULT_FFC_OUTPUT_FILE = 'full_noise0.featureXML'
 DEFAULT_IDM_OUTPUT_FILE = 'out_IDM.featureXML'
 WORKING_INI_FFC_FILE = 'feature_finder_custom.ini'
 WORKING_INI_IDM_FILE = 'id_mapper_custom.ini'
+SAVED_INI_FFC_FILE = 'save_ffc.ini'
+SAVED_INI_IDM_FILE = 'save_idm.ini'
 
 
 class Singleton:
@@ -64,6 +66,7 @@ class ConfigurationOptimiser():
         self.config = config
         self.optimisation_elements = []
         self.working_ini_file = None
+        self.ini_to_save = None
         self.lower_bound = None
         self.upper_bound = None
 
@@ -111,10 +114,10 @@ class ConfigurationOptimiser():
         with open(file_name, 'w') as f:
             f.write(self.dom.toxml())
 
-    def run_program(self, verbose=True):
+    def run_program(self, args, verbose=True):
         tries = 5
         while tries > 0:
-            p = Popen(self.get_args(), stdout=PIPE)
+            p = Popen(args, stdout=PIPE)
             res = p.wait()
             output = p.stdout.read().decode()
             if verbose:
@@ -143,84 +146,103 @@ class ConfigurationOptimiser():
             restrictions.append(1)
         else:
             restrictions = [float(restriction) for restriction in restrictions]
-            restrictions.append((restrictions[1] - restrictions[0])/100)
+            restrictions.append((restrictions[1] - restrictions[0])/20)
         return restrictions
 
-    def find_best_value(self, name, offset, *args):
+
+    def find_best_value(self, name, offset): # , *args):
         restrictions = self.get_restrictions(name, offset)
 
         self.write_config(self.working_ini_file)
         val = float(self.get_element(name, offset).getAttribute('value'))
         log("Trying with default: {} = {}...".format(name, val))
         print("Trying with default: {} = {}...".format(name, val))
-        output = self.run_program(False)
         res = None
-        try:
-            res = self.get_result(output)
-        except Exception as e:
-            log("fail: {}".format(str(e)))
-            print("fail: {}".format(str(e)))
+        for i in range(len(self.config.mzml)):
+            output = self.run_program(self.get_args(i), False)
+            try:
+                tmp_res = self.get_result(output, i)
+                res = self.add_res(res, tmp_res) if res is not None else tmp_res
+            except Exception as e:
+                log("fail: {}".format(str(e)))
+                print("fail: {}".format(str(e)))
 
         print("Result = {} with value = {}".format(res, val))
         log("Result = {} with value = {}".format(res, val))
 
         best_val = val
         best_res = res
-        val = restrictions[0]
-        step = restrictions[2]
-        other_best = []
-        other = []
+
+#        other_best = []
+#        other = []
 
         results = {}
 
-        while val <= restrictions[1]:
-            self.set_attribute(name, 'value', val, offset)
-            self.write_config(self.working_ini_file)
-            print("Trying {} = {}...".format(name, val))
-            log("Trying {} = {}...".format(name, val))
-            if len(args) > 0:
-                res, other = self.find_best_value(args[0], args[1], args[2:] if len(args) > 2 else None)
+        def one_search(low, up, step):
+            nonlocal best_res
+            nonlocal best_val
+            val = low
+            while val <= up:
+                self.set_attribute(name, 'value', val, offset)
+                self.write_config(self.working_ini_file)
+                print("Trying {} = {}...".format(name, val))
+                log("Trying {} = {}...".format(name, val))
+    #            if len(args) > 0:
+    #                res, other = self.find_best_value(args[0], args[1], args[2:] if len(args) > 2 else None)
+    #            else:
+                res = None
+                for i in range(len(self.config.mzml)):
+                    output = self.run_program(self.get_args(i), False)
+                    try:
+                        tmp_res = self.get_result(output, i)
+                        res = self.add_res(res, tmp_res) if res is not None else tmp_res
+                    except Exception as e:
+                        print("fail: {}".format(str(e)))
+                        log("fail: {}".format(str(e)))
+
+                results[val] = res
+
+                print("Result = {} with value = {}".format(res, val))
+                log("Result = {} with value = {}".format(res, val))
+
+                if self.cmp_result(res, best_res):
+                    best_res = res
+                    best_val = val
+
+                    if self.get_element(name, offset).getAttribute('type') != 'int':
+                        print("We need to go deeper with low = {}, high = {}, step = {}"
+                              "".format(val - step * 0.8, val + step * 0.8, step / 5))
+                        log("We need to go deeper with low = {}, high = {}, step = {}"
+                            "".format(val - step * 0.8, val + step * 0.8, step / 5))
+                        one_search(val - step, val + step, step / 5)
+    #                other_best = other
+
+                # print("Best = {} with value = {}".format(best_res, best_val))
+                # log("Best = {} with value = {}".format(best_res, best_val))
+
+                val += step
+
+        one_search(restrictions[0], restrictions[1], restrictions[2])
+
+#        if len(args) == 0:
+        self.lower_bound = restrictions[0]
+        self.upper_bound = restrictions[1]
+        upper = False
+        for val, res in sorted(results.items()):
+            print(val, res)
+            if upper:
+                if not self.low_result(res, best_res):
+                    self.upper_bound = val
             else:
-                output = self.run_program(False)
-                try:
-                    res = self.get_result(output)
-                except Exception as e:
-                    print("fail: {}".format(str(e)))
-                    log("fail: {}".format(str(e)))
+                if not self.low_result(res, best_res):
+                    self.lower_bound = val
+                    upper = True
 
-            results[val] = res
+        self.set_attribute(name, 'restrictions', '{}:{}'.format(self.lower_bound, self.upper_bound), offset)
+        self.set_attribute(name, 'value', best_val, offset)
 
-            if self.cmp_result(res, best_res):
-                best_res = res
-                best_val = val
-                other_best = other
-
-            print("Result = {} with value = {}".format(res, val))
-            log("Result = {} with value = {}".format(res, val))
-            # print("Best = {} with value = {}".format(best_res, best_val))
-            # log("Best = {} with value = {}".format(best_res, best_val))
-
-            val += step
-
-        if len(args) == 0:
-            self.lower_bound = restrictions[0]
-            self.upper_bound = restrictions[1]
-            upper = False
-            for val, res in sorted(results.items()):
-                print(val, res)
-                if upper:
-                    if not self.low_result(res, best_res):
-                        self.upper_bound = val
-                else:
-                    if not self.low_result(res, best_res):
-                        self.lower_bound = val
-                        upper = True
-
-            self.set_attribute(name, 'restrictions', '{}:{}'.format(self.lower_bound, self.upper_bound), offset)
-            self.set_attribute(name, 'value', best_val, offset)
-
-        other_best.insert(0, best_val)
-        return best_res, other_best
+#        other_best.insert(0, best_val)
+        return best_res, best_val #, other_best
 
     @abc.abstractmethod
     def low_result(self, res, best):
@@ -231,25 +253,35 @@ class ConfigurationOptimiser():
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_result(self, output):
+    def get_result(self, output, i):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_args(self):
+    def get_args(self, i=0):
         raise NotImplementedError
 
     def set_attribute(self, name, attr, value, offset):
         self.get_element(name, offset).setAttribute(attr, str(value))
 
+    @abc.abstractmethod
+    def pre_loading(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def add_res(self, res1, res2):
+        raise NotImplementedError
+
     def run(self):
+        self.pre_loading()
+
         for name, offset in self.optimisation_elements:
             try:
                 print("Optimising {}".format(name, offset))
                 log("Optimising {}".format(name, offset))
                 res, val = self.find_best_value(name, offset)
-                print("Best {} = {} with value = {}".format(name, res, val[0]))
-                log("Best {} = {} with value = {}".format(name, res, val[0]))
-                self.write_config("saved_config.ini")
+                print("Best {} = {} with value = {}".format(name, res, val))
+                log("Best {} = {} with value = {}".format(name, res, val))
+                self.write_config(self.ini_to_save)
             except Exception as e:
                 print("Can not find best value for {}: {}".format(name, e))
                 log("Can not find best value for {}: {}".format(name, e))
@@ -268,12 +300,14 @@ class FeatureFinderOptimiser(ConfigurationOptimiser):
                                       ('min_score', 1), ('min_isotope_fit', 0), ('min_trace_score', 0),
                                       ('min_rt_span', 0), ('max_rt_span', 0), ('max_intersection', 0)]
         self.working_ini_file = WORKING_INI_FFC_FILE
+        self.ini_to_save = SAVED_INI_FFC_FILE
         self.dom = xml.dom.minidom.parse(config.ffcini)
 
-    def get_args(self):
-        return self.config.ffc, '-in', self.config.mzml, '-out', DEFAULT_FFC_OUTPUT_FILE, '-ini', WORKING_INI_FFC_FILE
+    def get_args(self, i=0):
+        return self.config.ffc, '-in', self.config.mzml[i], '-out', str(i)+'_'+DEFAULT_FFC_OUTPUT_FILE,\
+               '-ini', WORKING_INI_FFC_FILE
 
-    def get_result(self, output):
+    def get_result(self, output, i):
         m = re.search(r"FeatureFinderCentroided took (\d*\.?\d*) s \(wall\)", output)
         if m:
             t = float(m.group(1))
@@ -285,8 +319,9 @@ class FeatureFinderOptimiser(ConfigurationOptimiser):
                 raise Exception("No result")
         opt = IDMapperOptimiser(self.config)
         opt.write_config(opt.working_ini_file)
-        output = opt.run_program(False)
-        return {'value': opt.get_result(output), 'time': t}
+        args = opt.get_args(i)
+        output = opt.run_program(args, False)
+        return {'value': opt.get_result(output, i), 'time': t}
         # return opt.get_result(output)
 
     def cmp_result(self, res, best):
@@ -295,19 +330,26 @@ class FeatureFinderOptimiser(ConfigurationOptimiser):
     def low_result(self, res, best):
         return res['value'] < 0.99 * best['value']
 
+    def pre_loading(self):
+        pass
+
+    def add_res(self, res1, res2):
+        return {'value': res1['value'] + res2['value'], 'time': res1['time'] + res2['time']}
+
 
 class IDMapperOptimiser(ConfigurationOptimiser):
     def __init__(self, config):
         ConfigurationOptimiser.__init__(self, config)
         self.optimisation_elements = [('rt_tolerance', 0), ('mz_tolerance', 0)]
         self.working_ini_file = WORKING_INI_IDM_FILE
+        self.ini_to_save = SAVED_INI_IDM_FILE
         self.dom = xml.dom.minidom.parse(config.idmini)
 
-    def get_args(self):
-        return (self.config.idm, '-in', DEFAULT_FFC_OUTPUT_FILE, '-out', DEFAULT_IDM_OUTPUT_FILE, '-id',
+    def get_args(self, i=0):
+        return (self.config.idm, '-in', str(i)+'_'+DEFAULT_FFC_OUTPUT_FILE, '-out', DEFAULT_IDM_OUTPUT_FILE, '-id',
                 self.config.idxml, '-ini', WORKING_INI_IDM_FILE)
 
-    def get_result(self, output):
+    def get_result(self, output, i):
         m = re.search(r"Peptides assigned to exactly one feature: (\d*)", output)
         if m:
             return int(m.group(1))
@@ -320,6 +362,22 @@ class IDMapperOptimiser(ConfigurationOptimiser):
     def low_result(self, res, best):
         return False
 
+    def pre_loading(self):
+        for i in range(len(self.config.mzml)):
+            args = (self.config.ffc, '-in', self.config.mzml[i], '-out', str(i)+'_'+DEFAULT_FFC_OUTPUT_FILE,
+                   '-ini', WORKING_INI_FFC_FILE)
+            self.run_program(args)
+
+    def add_res(self, res1, res2):
+        return res1 + res2
+
+
+def files(s):
+    try:
+        return s.split()
+    except:
+        raise argparse.ArgumentTypeError("files should be divided by commas")
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Settings selection for FeatureFinderCentroided algorithm')
@@ -327,7 +385,7 @@ def parse_args():
     parser.add_argument('-ffcini', type=str, help='Settings ini file for FeatureFinderCentroided')
     parser.add_argument('-idm', type=str, help='IDMapper executable')
     parser.add_argument('-idmini', type=str, help='Settings ini file for IDMapper')
-    parser.add_argument('-mzml', type=str, help='.mzML input file for FeatureFinderCentroided')
+    parser.add_argument('-mzml', type=files, help='.mzML input file for FeatureFinderCentroided')
     parser.add_argument('-idxml', type=str, help='.idXML input file for IDMapper')
     return parser.parse_args()
 
@@ -335,7 +393,7 @@ def parse_args():
 def main():
     config = parse_args()
     opt = FeatureFinderOptimiser(config)
-    # opt = IDMapperOptimiser(config)
+    opt1 = IDMapperOptimiser(config)
 
     # opt.write_config(opt.working_ini_file)
     # output = opt.run_program()
@@ -344,6 +402,9 @@ def main():
 
     while True:
         opt.run()
+        opt1.config.ffcini = SAVED_INI_FFC_FILE
+        opt1.run()
+        opt.config.idmini = SAVED_INI_IDM_FILE
 
 if __name__ == '__main__':
     main()
